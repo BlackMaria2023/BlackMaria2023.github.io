347bf6fa8262a056294dc93fc76512245a022412
mariajose <mariajosevargas605@gmail.com>
2024-02-15 10:59:56 -0300

    orden y otros cambios

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // Add smooth scrolling to all links
   $("a").on('click', function (event) {
-
+    
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -14,21 +14,34 @@ $(document).ready(function () {
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 1600, function () {
+      }, 400, function () {
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     } // End if
+
+
   });
+
+  $("#mostrarAlerta").click(function(){
+    alert("correo enviado")
+  }) 
+
+
 });
 
-//tooltip
-$(function () {
-  $(document).tooltip();
-});
+
+  
 
 
+
+
+
+
+
+  
+  
 
 
 
